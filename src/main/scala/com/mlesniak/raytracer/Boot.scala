5da1011f5d34e1d0226ec4c75a9ce265312b5031
Michael Lesniak <mlesniak@micromata.de>
2016-04-09 23:15:18 +0200

    Write pixels to file

--- src/main/scala/com/mlesniak/raytracer/Boot.scala
+++ src/main/scala/com/mlesniak/raytracer/Boot.scala
@@ -1,16 +1,26 @@
 package com.mlesniak.raytracer
 
+import java.awt.Color
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
 /**
   * Application entry point.
   *
   * @author Michael Lesniak (mlesniak@micromata.de)
   */
 object Boot extends App {
-  println("Hello, raytracing world")
+  println("Start")
 
-  (0 until 320).foreach { y =>
-    (0 until 200).foreach { x =>
-      println(s"y=$y x=$x")
+  // Draw single pixels.
+  val size = (320, 200)
+  val canvas = new BufferedImage(size._1, size._2, BufferedImage.TYPE_INT_RGB)
+  (0 until size._2).foreach { y =>
+    (0 until size._1).foreach { x =>
+      canvas.setRGB(x, y, (Math.random() * Int.MaxValue).asInstanceOf[Int])
     }
   }
+
+  ImageIO.write(canvas, "png", new java.io.File("image.png"))
+  println("Finish")
 }
